ddef561059bc54582e1cb7c9a2a42510e3a65c6a
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-04-30 09:35:07 +0300

    add gallery section

--- app/scripts/swiper.js
+++ app/scripts/swiper.js
@@ -12,6 +12,37 @@
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
-        }
+        },
+    });
+})();
+
+(function () {
+    const gallerySlider = document.querySelector(".gallery__slider");
+    if (!gallerySlider) return;
+
+    new Swiper(".gallery__slider", {
+        spaceBetween: 30,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".gallery .sl-arrows__arrow--next",
+            prevEl: ".gallery .sl-arrows__arrow--prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.3,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2.2,
+                spaceBetween: 30,
+            },
+            980: {
+                slidesPerView: 4,
+            },
+        },
     });
 })();
